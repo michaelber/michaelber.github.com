1be2d2592810462d91338f5eae3f64e436395c7c
Michael Bertsch <michael.bertsch@gmx.at>
2018-12-24 14:34:08 +0100

    Simplify + update Design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 jQuery(function($) {
 	$('.securemail').each(function(index) {
 	    var address = $(this).text();
@@ -8,26 +9,11 @@ jQuery(function($) {
 		   location.href = "mailto:" + address;
 		});
 	});
-
-	$('#home a').mouseover(function() {
-		$(this).fadeTo('fast', 1);
-		$('.coda-nav-left, .coda-nav-right').fadeTo('fast', 1);
-	});
-	$('#home a').mouseout(function() {
-		$(this).fadeTo('fast', 0.7);
-		$('.coda-nav-left, .coda-nav-right').fadeTo('fast', 0.6);
-	});		
-	
-	$('#slider').codaSlider();
-	
-	
-	// Get currentPanel height to set marginTop (center positioning)
-	if(!location.hash) {
-		currentPanel = 0;
-		$('.coda-nav-left, .coda-nav-right').hide();
-	} else {
-		currentPanel = parseInt(location.hash.slice(1))-1;
-	}
-	panelHeight = $('.panel:eq(' + currentPanel + ')', $('#slider')).height();
-	$('#slider').css({ marginTop: -(panelHeight/2) });
 });
+*/
+
+function securemail(m) {
+	 m = m.replace('(at)','@').replace('(dot)','.');  
+	 window.location.href = "mailto:"+m;
+  return false;
+}
